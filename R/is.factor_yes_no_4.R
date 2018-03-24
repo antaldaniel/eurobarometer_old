e59b763d650eae7822367c00d795945bfdb74cb6
antaldaniel <antaldaniel@gmail.com>
2018-03-24 18:02:05 +0100

    naming inconsistency

--- R/is.factor_yes_no_4.R
+++ R/is.factor_yes_no_4.R
@@ -3,10 +3,10 @@
 #' @param x An object to be checked if it is a 4-level (2 positive,
 #' 2 negative) categorical variable.
 #' @examples
-#' my_categories <- factor_yes_no_4( c("Very good", "Rather Good",
+#' my_categories <- as_factor_yes_no_4( c("Very good", "Rather Good",
 #'                   "Inap.", "Rather Bad", "DK", "Very bad"))
 #'
-#' is.factor_yes_no_4 (my_binary)
+#' is.factor_yes_no_4 (my_categories)
 #'
 #' @export
 #'
