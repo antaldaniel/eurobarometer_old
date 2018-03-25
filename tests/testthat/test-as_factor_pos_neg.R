04c119a66cc81186d7617996db9b26b90c1223f0
antaldaniel <antaldaniel@gmail.com>
2018-03-25 14:07:53 +0200

    adding unit tests

---
+++ tests/testthat/test-as_factor_pos_neg.R
@@ -0,0 +1,28 @@
+library (surveyreader)
+library(testthat)
+context("Correct class conversion to positive neutral negative")
+
+test_that("correct conversion takes place", {
+  expect_equal(as_factor_pos_neg (c("Better", "DK", "Worse",
+                                    "Same", "The Same",
+                                    "Inap. not "))[[3]],
+  c( 1, NA, -1, 0, 0,NA )
+  )
+})
+
+test_that("class factor_pos_neg is recognized", {
+  expect_equal(is.factor_pos_neg(as_factor_pos_neg (c("Better", "DK", "Worse",
+                                    "Same", "The Same",
+                                    "Inap. not "))),
+               TRUE
+  )
+})
+
+
+test_that("correct as_numeric method", {
+  expect_equal(as_numeric(as_factor_pos_neg (c("Better", "DK", "Worse",
+                                    "Same", "The Same",
+                                    "Inap. not "))),
+               c( 1, NA, -1, 0, 0,NA )
+  )
+})
---
+++ tests/testthat/test-as_factor_yes_no_4.R
@@ -0,0 +1,33 @@
+library (surveyreader)
+library(testthat)
+context("Correct class conversion to positive neutral negative")
+
+
+as_factor_yes_no_4( c("Very good", "Rather Good",
+                      "Inap.", "Rather Bad", "DK",
+                      "Very bad"))
+test_that("correct conversion takes place", {
+  expect_equal(as_factor_yes_no_4 (c("Very good", "Rather Good",
+                                     "Inap.", "Rather Bad", "DK",
+                                     "Very bad"))[[3]],
+  c( -2, -1, NA, 1, NA, 2 )
+  )
+})
+
+test_that("class factor_pos_neg is recognized", {
+  expect_equal(is.factor_yes_no_4(
+     as_factor_yes_no_4(c("Very good", "Rather Good",
+                                   "Inap.", "Rather Bad", "DK",
+                                   "Very bad"))),
+               TRUE
+  )
+})
+
+
+test_that("correct as_numeric method", {
+  expect_equal(as_numeric(as_factor_yes_no_4 (c("Very good", "Rather Good",
+                                                "Inap.", "Rather Bad", "DK",
+                                                "Very bad"))),
+               c( -2, -1, NA, 1, NA, 2 )
+  )
+})
