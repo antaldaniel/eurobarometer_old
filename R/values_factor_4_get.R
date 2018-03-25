52f1a39ab88afe511be01f1d1ce96a51e7543691
antaldaniel <antaldaniel@gmail.com>
2018-03-25 17:24:40 +0200

    Interested

--- R/values_factor_4_get.R
+++ R/values_factor_4_get.R
@@ -10,18 +10,23 @@ values_factor_4_get <- function () {
   factor_4_0 <- c("Not at all",
                   "Not at all informed",
                   "Not at all likely",
+                  "Not at all interested",
                   "Not at all interested in politics",
                   "Not at all interested in politics")
   factor_4_1 <- c("Low",
-                  "Not very informed", "Not very likely",
+                  "Not very informed",
+                  "Not very likely",
+                  "Not very interested",
                   "Slightly interested in politics"
                   )
   factor_4_2 <- c("Medium",
                   "Fairly well informed", "Fairly likely",
+                  "Fairly interested",
                   "Moderately interested in politics")
   factor_4_3 <- c("Strong",
                   "Very well informed",
                   "Very likely",
+                  "Very interested",
                   "Strongly interested in politics")
 
   factor_4_list <- list ( factor_4_0, factor_4_1,
