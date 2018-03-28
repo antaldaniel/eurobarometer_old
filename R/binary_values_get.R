526b3bb791d3d6011f8c7825c718e663974b78f1
antaldaniel <antaldaniel@gmail.com>
2018-03-28 10:25:53 +0200

    from system.rda

--- R/binary_values_get.R
+++ R/binary_values_get.R
@@ -9,27 +9,5 @@
 #'
 
 binary_values_get <- function () {
-  yes_values <- c("igen", "yes", "mentioned",
-                  "da", "agree", "for",
-                  "tend to trust",
-                  "woud benefit",
-                  "has benefited",
-                  "female", "Woman",
-                  "Doing a good job",
-                  "Correct answer",
-                  "true",
-                  "True (correct)",
-                  "correct")
-  no_values <- c("nem", "no", "not mentioned",
-                 "net", "disagree", "against",
-                 "tend not to trust",
-                 "would not benefit",
-                 "has not benefited", "male", "Man",
-                 "Not doing a good job",
-                 "Wrong answer",
-                 "false",
-                 "false (incorrect)",
-                 "incorrect")
-  binary_list <- list ( yes_values,  no_values)
-  return(binary_list)
+    return(values_factor_binary)
 }
