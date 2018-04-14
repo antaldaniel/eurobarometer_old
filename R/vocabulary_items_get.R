402ef574366f5a740ae591dde9bf344de88c7ff5
antaldaniel <antaldaniel@gmail.com>
2018-04-14 09:21:06 +0200

    improving the documentation type filtering

--- R/vocabulary_items_get.R
+++ R/vocabulary_items_get.R
@@ -2,14 +2,23 @@
 #'
 #' Get the vocabulary used for re-coding or reordering
 #' categorical variables
+#' @importFrom type Filtering for the vocabulary of a class only, such as the
+#' vocabulary of only 3-level \code{factor_3} variables. Defaults to \code{NULL}.
+#' @importFrom dplyr filter
+#' @importFrom magrittr '%>%'
 #' @examples
 #' vocabulary_items_get()
 #' @export
 
 vocabulary_items_get <- function ( type = NULL) {
-  load(vocabulary)
-
-  return (vocabulary)
+  if (is.null(type)) { return (vocabulary) }
+  if (type %in% unique (vocabulary$context)) {
+    voc <- vocabulary %>%
+      dplyr::filter ( context ==  !! type  )
+    return (voc)
+  } else {
+    stop("Error: ", type, " not recognized in the vocabulary file.")
+  }
 }
 
 
