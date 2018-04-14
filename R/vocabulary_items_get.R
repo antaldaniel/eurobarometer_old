e5b3c9b9c38cc7a6320d4f48b903ee20b87a03b0
antaldaniel <antaldaniel@gmail.com>
2018-04-14 09:23:33 +0200

    paramter renaming

--- R/vocabulary_items_get.R
+++ R/vocabulary_items_get.R
@@ -2,7 +2,7 @@
 #'
 #' Get the vocabulary used for re-coding or reordering
 #' categorical variables
-#' @importFrom type Filtering for the vocabulary of a class only, such as the
+#' @param context Filtering for the vocabulary of a class only, such as the
 #' vocabulary of only 3-level \code{factor_3} variables. Defaults to \code{NULL}.
 #' @importFrom dplyr filter
 #' @importFrom magrittr '%>%'
@@ -10,14 +10,14 @@
 #' vocabulary_items_get()
 #' @export
 
-vocabulary_items_get <- function ( type = NULL) {
-  if (is.null(type)) { return (vocabulary) }
-  if (type %in% unique (vocabulary$context)) {
+vocabulary_items_get <- function ( context = NULL) {
+  if (is.null(context)) { return (vocabulary) }
+  if (context %in% unique (vocabulary$context)) {
     voc <- vocabulary %>%
-      dplyr::filter ( context ==  !! type  )
+      dplyr::filter ( context ==  !! context  )
     return (voc)
   } else {
-    stop("Error: ", type, " not recognized in the vocabulary file.")
+    stop("Error: ", context, " not recognized in the vocabulary file.")
   }
 }
 
